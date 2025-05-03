1be1d549eeb52fe1c3f26ec4acd068fb9fa5c3af
jorgefz <jorgefz.fernandez@gmail.com>
2025-05-03 09:33:53 +0100

    Added extras code to test collision rate

--- extras/test_hashmap_collisions.c
+++ extras/test_hashmap_collisions.c
@@ -11,25 +11,28 @@
 
 int main(){
 
-    srand(time(NULL));
+    srand((unsigned int)time(NULL));
 
     static char keys[NKEYS][KEYLEN + 1];
 
     // Generate keys
     for(uint32_t i = 0; i != NKEYS; ++i){
         for(uint32_t j = 0; j != KEYLEN; ++j){
-            keys[i][j] = (char)(rand() % 256);
+            keys[i][j] = (char)(  rand() % ('z' + 1 - 'a') + 'a' );
         }
         keys[i][KEYLEN] = '\0';
     }
+    printf("Keys generated\n");
 
     hashmap_t map;
-    hashmap_init(&map, NKEYS*2);
+    void* p = hashmap_init(&map, NKEYS*10);
+    printf("Map initialised\n");
 
     // Insert keys
     for(uint32_t i = 0; i != NKEYS; ++i){
-        hashmap_setb(&map, keys[i], KEYLEN, NULL);
+        hashmap_setb(&map, keys[i], KEYLEN+1, NULL);
     }
+    printf("Keys inserted\n");
 
     // Scan for collisions
     hashmap_entry_t* entry;
@@ -37,15 +40,29 @@ int main(){
     for(uint64_t i = 0; i != map.size; ++i){
         entry = map.table[i];
         if(entry && entry->next){
+            printf("Collision of %s with ", entry->key);
+            entry = entry->next;
+            while (entry) {
+                printf(" %s, ", entry->key);
+                entry = entry->next;
+            }
+            printf("\n");
             collisions++;
         }
     }
-    hashmap_uninit(&map);
 
     // Stats
-    double loadf = (double)NKEYS / (double)map.size;
-    printf("Keys %u and map size %lu (loading factor %.2g)\n", NKEYS, map.size, loadf);
+    dast_sz mapsz = map.size;
+    double loadf = (double)NKEYS / (double)mapsz;
+    printf("Keys %u and map size %u (loading factor %.2g)\n", NKEYS, (unsigned int)map.size, loadf);
     printf("Collisions: %u\n", collisions);
+
+    hashmap_uninit(&map);
+
+    // Hashing test
+    dast_u64 h1 = hashmap_FNV1a64_hash("danhuugadk", KEYLEN+1) % mapsz;
+    dast_u64 h2 = hashmap_FNV1a64_hash("ympzximqqj", KEYLEN+1) % mapsz;
+    printf("%llu = %llu (%d)\n", h1, h2, h1==h2);
 
     return 0;
 }
