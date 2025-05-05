8d3257ed55ce80c03e7bbb7ace85425778171d33
jorgefz <jorgefz.fernandez@gmail.com>
2025-05-05 13:04:58 +0100

    Changed C dialect to C99 for all projects

--- premake5.lua
+++ premake5.lua
@@ -26,18 +26,20 @@ workspace "dast"
 project "dast"
     kind "StaticLib"
     language "C"
-    cdialect "C89"
+    cdialect "C99"
     staticruntime "on"
     location "build/%{prj.name}"
     objdir ("obj/" .. OutputDir .. "/%{prj.name}" )
     targetdir ("bin/" .. OutputDir .. "/%{prj.name}" )
     files { "src/**.c", "include/**.h" }
     includedirs { "include" }
+    filter { "system:linux", "action:gmake2" }
+        buildoptions {"-pedantic" }
 
 project "test"
     kind "ConsoleApp"
     language "C"
-    cdialect "C11"
+    cdialect "C99"
     location "build/%{prj.name}"
     objdir ("obj/" .. OutputDir .. "/%{prj.name}" )
     targetdir ("bin/" .. OutputDir .. "/%{prj.name}" )
@@ -53,7 +55,7 @@ project "test"
 project "extras"
     kind "ConsoleApp"
     language "C"
-    cdialect "C11"
+    cdialect "C99"
     location "build/%{prj.name}"
     objdir ("obj/" .. OutputDir .. "/%{prj.name}" )
     targetdir ("bin/" .. OutputDir .. "/%{prj.name}" )
